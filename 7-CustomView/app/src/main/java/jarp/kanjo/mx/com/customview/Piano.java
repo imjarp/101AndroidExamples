ac6e6a8378fde792ca8e7263bde0ae13db85d032
JARP <ramirezp.jorge@gmail.com>
2015-04-02 19:07:15 -0600

    adding keys

--- 7-CustomView/app/src/main/java/jarp/kanjo/mx/com/customview/Piano.java
+++ 7-CustomView/app/src/main/java/jarp/kanjo/mx/com/customview/Piano.java
@@ -2,6 +2,7 @@ package jarp.kanjo.mx.com.customview;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -62,15 +63,73 @@ public class Piano extends View {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         mPointerCoords = new MotionEvent.PointerCoords();
         Arrays.fill(mFingerPoints, null);
         Arrays.fill(mFingerTones, -1);
-
+        loadKeySamples(getContext());
+        setupPaints();
         
     }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        releaseKeySamples();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        //Calculate the sizes for the keys
+        int width = getWidth();
+        int heigth = getHeight();
+
+        int whiteKeyWidth = width / WHITE_KEYS_COUNT;
+        int blackKeyWidth = (int) ( whiteKeyWidth * BLACK_TO_WHITE_WIDTH_RATIO  );
+        int blackKeyHeight = (int) ( heigth * BLACK_TO_WHITE_HEIGHT_RATIO );
+
+        //Define rectangles
+        // set(int left, int top, int right, int bottom) {
+        mCKey.set(0,0,whiteKeyWidth,heigth);
+        mDKey.set(whiteKeyWidth,0,whiteKeyWidth*2,heigth);
+        mEKey.set(whiteKeyWidth * 2,0,whiteKeyWidth*3,heigth);
+        mFKey.set(whiteKeyWidth * 3,0,whiteKeyWidth*4,heigth);
+        mGKey.set(whiteKeyWidth * 4,0,whiteKeyWidth*5,heigth);
+        mAKey.set(whiteKeyWidth * 5,0,whiteKeyWidth*6,heigth);
+        mBKey.set(whiteKeyWidth * 6,0,whiteKeyWidth*7,heigth);
+
+
+
+
+        mCSharpKey.set( whiteKeyWidth - ( blackKeyWidth / 2 ) ,0 ,whiteKeyWidth +  (blackKeyWidth / 2 ) , blackKeyHeight );
+        mDSharpKey.set( ( whiteKeyWidth * 2 ) + ( blackKeyWidth / 2 ) , 0 , whiteKeyWidth * 2 +  (blackKeyWidth / 2 ) , blackKeyHeight );
+
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(mCKey,mCKeyPaint);
+        canvas.drawRect(mDKey,mDKeyPaint);
+        canvas.drawRect(mEKey,mEKeyPaint);
+        canvas.drawRect(mFKey,mFKeyPaint);
+        canvas.drawRect(mGKey,mGKeyPaint);
+        canvas.drawRect(mAKey,mAKeyPaint);
+        canvas.drawRect(mBKey,mBKeyPaint);
+
+        canvas.drawRect(mCSharpKey,mCSharpKeyPaint);
+        canvas.drawRect(mDSharpKey,mDSharpKeyPaint);
+    }
+
+
+
 
 
     private void setupPaints() {
@@ -106,6 +165,8 @@ public class Piano extends View {
     }
 
     private void loadKeySamples(Context context) {
+
+
         mSoundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
         mToneToIndexMap.put(R.raw.c1, mSoundPool.load(context, R.raw.c1, 1));
         mToneToIndexMap.put(R.raw.c1s, mSoundPool.load(context, R.raw.c1s, 1));
@@ -120,4 +181,15 @@ public class Piano extends View {
         mToneToIndexMap.put(R.raw.a1s, mSoundPool.load(context, R.raw.a1s, 1));
         mToneToIndexMap.put(R.raw.b1, mSoundPool.load(context, R.raw.b1, 1));
     }
+
+    private void releaseKeySamples(){
+
+        mToneToIndexMap.clear();
+        mSoundPool.release();
+
+    }
+
+
 }
+
+
